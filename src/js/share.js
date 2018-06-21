1ccfd1749c3bcf832d18f5bdf26a285fee5edd84
Steven <1977744311@qq.com>
2018-06-21 12:28:20 +0800

    q

--- src/js/share.js
+++ src/js/share.js
@@ -1,12 +1,12 @@
 Vue.component('share',{
-    props:['shareLink'],
+    props:['sharelink'],
     template:`
     <div class="share" v-cloak>
         <h2>
             请将下面链接分享给面试官
         </h2>
         <div>
-            <textarea readonly>{{shareLink}}</textarea>
+            <textarea readonly>{{sharelink}}</textarea>
         </div>
     </div>
     `
